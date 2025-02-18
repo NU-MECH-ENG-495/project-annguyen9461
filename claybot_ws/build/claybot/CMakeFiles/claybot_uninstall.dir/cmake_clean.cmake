b3e3ac40efcaf8f43f1d1102b1e66f682bb120c8
An Nguyen <annguyen2025@u.northwestern.edu>
2025-02-18 10:31:15 -0600

    created package and one node for vision. Updated READDME

---
+++ claybot_ws/build/claybot/CMakeFiles/claybot_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/claybot_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/claybot_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
