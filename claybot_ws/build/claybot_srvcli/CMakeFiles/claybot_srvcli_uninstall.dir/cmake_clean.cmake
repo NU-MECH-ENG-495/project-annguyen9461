9a9c2dea1b3c4778669e45cf6624fadaf168000c
An Nguyen <annguyen2025@u.northwestern.edu>
2025-02-18 11:25:34 -0600

    simple client service nodes and new package for custom interface

---
+++ claybot_ws/build/claybot_srvcli/CMakeFiles/claybot_srvcli_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/claybot_srvcli_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/claybot_srvcli_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ claybot_ws/build/claybot_srvcli/CMakeFiles/claybot_srvcli_uninstall.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for claybot_srvcli_uninstall.
+# This may be replaced when dependencies are built.
