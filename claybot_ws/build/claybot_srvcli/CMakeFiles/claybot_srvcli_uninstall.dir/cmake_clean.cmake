4833844dccf269a0b0c70e046badbe5da26eb275
An Nguyen <annguyen2025@u.northwestern.edu>
2025-02-18 13:28:51 -0600

    can use example launch file

--- claybot_ws/build/claybot_srvcli/CMakeFiles/claybot_srvcli_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/claybot_srvcli_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/claybot_srvcli_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
