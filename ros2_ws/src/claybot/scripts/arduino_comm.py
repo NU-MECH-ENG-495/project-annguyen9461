15dc0043841f6d2b37d62e1f4544117d690166b4
An Nguyen <annguyen2025@u.northwestern.edu>
2025-03-19 17:13:02 -0500

    can detect arduino

--- ros2_ws/src/claybot/scripts/arduino_comm.py
+++ ros2_ws/src/claybot/scripts/arduino_comm.py
@@ -16,8 +16,6 @@ class InoComm(Node):
 
     Responsibilities:
     - Communicates with an Arduino device over a serial port.
-    - Handles client calls to start a game and process received messages.
-    - Interfaces with the `play` and `toggle_tf_publish` services.
 
     **Services**:
 
@@ -36,28 +34,30 @@ class InoComm(Node):
         """
         super().__init__('inocomm')
 
-        # ARDUINO_SERIAL = '24336303633351E0D100'
+        ARDUINO_SERIAL = '24336303633351E0D100'
 
-        # # ports discovery
-        # ports = list(list_ports.comports(True))  # get list of ports
-        # arduino_port = None
+        # ports discovery
+        ports = list(list_ports.comports(True))  # get list of ports
+        arduino_port = None
 
-        # # grab the port that's connected to the Arduino
-        # for p in ports:
-        #     if p.serial_number == ARDUINO_SERIAL:
-        #         arduino_port = p.device
-        #         break
+        # grab the port that's connected to the Arduino
+        for p in ports:
+            if p.serial_number == ARDUINO_SERIAL:
+                arduino_port = p.device
+                break
 
-        # # check Arduino has been found
-        # if arduino_port is None:
-        #     self.get_logger().error('error finding arduino')
+        # check Arduino has been found
+        if arduino_port is None:
+            self.get_logger().error('error finding arduino')
+        else:
+            self.get_logger().info('found arduino')
 
-        # self.connect_serial_port(serial_port=arduino_port, baud_rate=115200)
+        self.connect_serial_port(serial_port=arduino_port, baud_rate=115200)
 
-        self.publisher_ = self.create_publisher(String, 'topic', 10)
-        timer_period = 0.5  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = 0
+        # self.publisher_ = self.create_publisher(String, 'topic', 10)
+        # timer_period = 0.5  # seconds
+        # self.timer = self.create_timer(timer_period, self.timer_callback)
+        # self.i = 0
 
     def timer_callback(self):
         msg = String()
@@ -66,6 +66,19 @@ class InoComm(Node):
         self.get_logger().info('Publishing: "%s"' % msg.data)
         self.i += 1
 
+    def connect_serial_port(self, serial_port, baud_rate):
+        """
+        Configure and opens the serial port for communication with the Arduino device.
+
+        :arg serial_port: The name of the serial port to connect to.
+        :type serial_port: str
+        :arg baud_rate: The baud rate for serial communication.
+        :type baud_rate: int
+        """
+        arduino_comm.port = serial_port
+        arduino_comm.baudrate = baud_rate
+        arduino_comm.timeout = 1
+        arduino_comm.open()
 
 def main(args=None):
     """
